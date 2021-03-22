9704966bd335eaad50963437c4fdbf95dc2c0cd9
javierveraesan <17100829@ue.edu.pe>
2021-03-21 20:58:41 -0500

    02

--- index.js
+++ index.js
@@ -101,9 +101,9 @@ const {
   //--Kontak
   const vcard = 'BEGIN:VCARD\n'
   + 'VERSION:3.0\n'
-  + 'FN:Mr.Pato\n' // Nama
-  + 'ORG:Mr.Pato-Bot;\n' // Nama bot
-  + 'TEL;type=CELL;type=VOICE;waid=17693039228:+1 769 303-9223n' // Nomor bot
+  + 'FN:TerruBot0.1\n' // Nama
+  + 'ORG:TerruBot0.1;\n' // Nama bot
+  + 'TEL;type=CELL;type=VOICE;waid=12252775810:+1 225 277-5810n' // Nomor bot
   + 'END:VCARD' 
   
   
@@ -585,22 +585,19 @@ const {
   ────────────────
   〘 *BOT* 〙
   ‣ Prefijo:  *「 ${prefix} 」*
-  ‣ Yo soy: 𝕸𝖗.𝕻𝖆𝖙𝖔 -𝕭𝖔𝖙⚜️🦆
-  ‣ Conección: *Baileys*
+  ‣ Yo soy:  TerrucoBot0.1  
+  ‣ Conección: *TERRUCO CONNECT 2021*
   ‣ Tiempo encencido: *${kyun(uptime)}*
   ‣ Modo: *ON*
   ‣ Grupo:  *${groupName}*
   ‣ Número de grupos: *${_registered.length}*
   ‣ Número de chats: *${totalchat.length}*
-  ‣ Grupo oficial
-  • _${Wa1}_
-  ‣ Suscribete
-  _${Ig}_
+ 
   ────────────────
   ┏━━━━━━━━━━━━━━━━━━━━
-  ┃─────〘 𝕸𝖗.𝕻𝖆𝖙𝖔 -𝕭𝖔𝖙⚜️🦆 〙────
+  ┃─────〘 TERRUCOBOT 0.1 〙────
   ┃━━━━━━━━━━━━━━━━━━━━
-  ┠⊷️ *${prefix}Simi (Teks)*
+  ┠⊷️ *${prefix}Simi (Text)*
   ┃ _Simsimi Chat_
   ┠⊷️ *${prefix}Info*
   ┃ _Información del bot_
@@ -764,18 +761,18 @@ const {
   *〘 TEXT0 〙*
   ┏━━━━━━━━━━━━━━━━━━━━
   ┠⊷️ *${prefix}Tahta (Texto)*
-  ┃ _Ex : ${prefix}Tahta Mr.pato_
+  ┃ _Ex : ${prefix}Tahta TerruBot0.1_
   ┗━━━━━━━━━━━━━━━━━━━━
   *〘 TEXTO2 〙*
   ┏━━━━━━━━━━━━━━━━━━━━
   ┠⊷️ *${prefix}Ephoto list*
   ┃ _Lista de ephoto_
   ┠⊷️ *${prefix}Ephoto (code + Texto)*
-  ┃ _Ex : ${prefix}Ephoto 25 Mr.pato_
+  ┃ _Ex : ${prefix}Ephoto 25 TerruBot0.1_
   ┠⊷️ *${prefix}Textpro list*
   ┃ _Lista de textpro
   ┠⊷️ *${prefix}Textpro (code + Texto)*
-  ┃ _Ex : ${prefix}Textpro 55 Mr.pato_
+  ┃ _Ex : ${prefix}Textpro 55 TerruBot0.1_
   ┗━━━━━━━━━━━━━━━━━━━━
   *〘 IMAGENES 〙*
   ┏━━━━━━━━━━━━━━━━━━━━
@@ -885,14 +882,16 @@ const {
   ┃ _Anuncio_
   ┠⊷️ *${prefix}Blocklist*
   ┃ _Lista de usuarios bloqueados_
-  ┗━━━━━━━━━━━━━━━━━━━━`,
+  ┗━━━━━━━━━━━━━━━━━━━━
+  *TERRUCOS DE LA BEBA ARMY, CONCHA DE TU MADRE*
+  `,
   
   contextInfo: {
 	mentionedJid: [sender]
   }
 	}
 	Lxa.sendMessage(from, Menu, text, {
-  quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "𝕸𝖗.𝕻𝖆𝖙𝖔 -𝕭𝖔𝖙⚜️🦆", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": "/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEABERERESERMVFRMaHBkcGiYjICAjJjoqLSotKjpYN0A3N0A3WE5fTUhNX06MbmJiboyiiIGIosWwsMX46/j///8BERERERIRExUVExocGRwaJiMgICMmOiotKi0qOlg3QDc3QDdYTl9NSE1fToxuYmJujKKIgYiixbCwxfjr+P/////CABEIADoAUQMBIgACEQEDEQH/xAAsAAEAAwEBAQAAAAAAAAAAAAAAAgMFBAYBAQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIQAxAAAADwYAAAAAEo2EoeggYctbsPMX7XIZPycABZWNvLoHTfnjTo4x0c4AAAAAAAf//EAC0QAAMAAgEDAwIDCQAAAAAAAAECAwQRAAUSMRMhImGSFCCRJEBBQlBRUmOx/9oACAEBAAE/AP3dFLsqjW2IA2QB+p5PHvUoJxdy7FU7VJ7iPcgcM6AMxRtKwVjrwT4B4Y2Hdub/ABRXb28K2tE/Q74+Nead7yZV+BHcNbDglSPodcKsp0wIOgf14mLeisyKCFAJII/ipf8A4vCrKFJBAYbH1Hj8kXWdFdpJUD+RywB+0g8z8N4dXtiQgHxlo5STuyy2qbbmXTHxhjt2SZ6I4yISuzy/17IZudWImmKoT0bvH9pkHc+DpAQxbma/bTqUE2hwEAlUO/eQjiXMARsnTUvAX/EZxgS7vtUQIAF03OjXcdSxJnbLS0kPzddbPbsFCOUFQQKBge1dBv8AEjY/JJ1m4ZpJQDfwbYB+0g8yOvXybpZ8eHm21AfTesgRuXrOpUpjzj9ELnf3luWyTe+Tek0L2ZmPkBSx3teX6lW4uTKS0v7Wou9v7huY/VDj+l24sG9K5vLff8GOv7NzEy/wl43WEneWivd3a7g2w3sRzKyGybeqyKnwRAq70AihB5/on//EABQRAQAAAAAAAAAAAAAAAAAAAED/2gAIAQIBAT8AR//EABQRAQAAAAAAAAAAAAAAAAAAAED/2gAIAQMBAT8AR//Z", "scansSidecar": "1W0XhfaAcDwc7xh1R8lca6Qg/1bB4naFCSngM2LKO2NoP5RI7K+zLw==" } } } })
+  quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "TerrucoBot0.1", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": "/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEABERERESERMVFRMaHBkcGiYjICAjJjoqLSotKjpYN0A3N0A3WE5fTUhNX06MbmJiboyiiIGIosWwsMX46/j///8BERERERIRExUVExocGRwaJiMgICMmOiotKi0qOlg3QDc3QDdYTl9NSE1fToxuYmJujKKIgYiixbCwxfjr+P/////CABEIADoAUQMBIgACEQEDEQH/xAAsAAEAAwEBAQAAAAAAAAAAAAAAAgMFBAYBAQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIQAxAAAADwYAAAAAEo2EoeggYctbsPMX7XIZPycABZWNvLoHTfnjTo4x0c4AAAAAAAf//EAC0QAAMAAgEDAwIDCQAAAAAAAAECAwQRAAUSMRMhImGSFCCRJEBBQlBRUmOx/9oACAEBAAE/AP3dFLsqjW2IA2QB+p5PHvUoJxdy7FU7VJ7iPcgcM6AMxRtKwVjrwT4B4Y2Hdub/ABRXb28K2tE/Q74+Nead7yZV+BHcNbDglSPodcKsp0wIOgf14mLeisyKCFAJII/ipf8A4vCrKFJBAYbH1Hj8kXWdFdpJUD+RywB+0g8z8N4dXtiQgHxlo5STuyy2qbbmXTHxhjt2SZ6I4yISuzy/17IZudWImmKoT0bvH9pkHc+DpAQxbma/bTqUE2hwEAlUO/eQjiXMARsnTUvAX/EZxgS7vtUQIAF03OjXcdSxJnbLS0kPzddbPbsFCOUFQQKBge1dBv8AEjY/JJ1m4ZpJQDfwbYB+0g8yOvXybpZ8eHm21AfTesgRuXrOpUpjzj9ELnf3luWyTe+Tek0L2ZmPkBSx3teX6lW4uTKS0v7Wou9v7huY/VDj+l24sG9K5vLff8GOv7NzEy/wl43WEneWivd3a7g2w3sRzKyGybeqyKnwRAq70AihB5/on//EABQRAQAAAAAAAAAAAAAAAAAAAED/2gAIAQIBAT8AR//EABQRAQAAAAAAAAAAAAAAAAAAAED/2gAIAQMBAT8AR//Z", "scansSidecar": "1W0XhfaAcDwc7xh1R8lca6Qg/1bB4naFCSngM2LKO2NoP5RI7K+zLw==" } } } })
 	break
   
   //--Cek limit user
@@ -1146,7 +1145,7 @@ const {
   nimek = n[Math.floor(Math.random() * n.length)];
   pok = await getBuffer(nimek)
   Lxa.sendMessage(from, pok, image, {
-	quoted: mek, caption: `Mr.Pato-Bot`
+	quoted: mek, caption: `TerruBot0.1`
   })
   
   } catch {
@@ -1170,7 +1169,7 @@ const {
   nimek = n[Math.floor(Math.random() * n.length)];
   pok = await getBuffer(nimek)
   Lxa.sendMessage(from, pok, image, {
-	quoted: mek, caption: `Mr.Pato-Bot`
+	quoted: mek, caption: `TerruBot0.1`
   })
   
   } catch {
@@ -1224,7 +1223,7 @@ const {
   nimek = n[Math.floor(Math.random() * n.length)];
   pok = await getBuffer(nimek)
   Lxa.sendMessage(from, pok, image, {
-	quoted: mek, caption: `Mr.Pato-Bot`
+	quoted: mek, caption: `TerruBot0.1`
   })
   
   break
@@ -1249,7 +1248,7 @@ const {
 	nimek = n[Math.floor(Math.random() * n.length)];
 	pok = await getBuffer(nimek)
 	Lxa.sendMessage(from, pok, image, {
-  quoted: mek, caption: `Mr.Pato-Bot`
+  quoted: mek, caption: `TerruBot0.1`
 	})
 	
 	} catch {
@@ -1279,7 +1278,7 @@ const {
 	nimek = n[Math.floor(Math.random() * n.length)];
 	pok = await getBuffer(nimek)
 	Lxa.sendMessage(from, pok, image, {
-  quoted: mek, caption: `Mr.Pato-Bot`
+  quoted: mek, caption: `TerruBot0.1`
 	})
 	
 	} catch {
@@ -1305,7 +1304,7 @@ const {
 	nimek = n[Math.floor(Math.random() * n.length)];
 	pok = await getBuffer(nimek)
 	Lxa.sendMessage(from, pok, image, {
-  quoted: mek, caption: `Mr.Pato-Bot`
+  quoted: mek, caption: `TerruBot0.1`
 	})
 	
 	} catch {
@@ -1331,7 +1330,7 @@ const {
 	nimek = n[Math.floor(Math.random() * n.length)];
 	pok = await getBuffer(nimek)
 	Lxa.sendMessage(from, pok, image, {
-  quoted: mek, caption: `Mr.pato-Bot`
+  quoted: mek, caption: `TerruBot0.1`
 	})
 	
 	} catch {
@@ -3117,7 +3116,7 @@ const {
 	if (err) return reply('Error al convertir el sticker en vídeo')
 	buffer = fs.readFileSync(ran)
 	Lxa.sendMessage(from, buffer, video, {
-  quoted: mek, caption: 'Mr.Pato'
+  quoted: mek, caption: 'TerruBot0.1'
 	})
 	fs.unlinkSync(ran)
   })
@@ -3521,7 +3520,7 @@ const {
 						  fs.unlinkSync(medimg)
 						  if (err) return reply('No se pudo hacer :(')
 						  buffer = fs.readFileSync(ran)
-						  Lxa.sendMessage(from, buffer, image, {quoted: mek, caption: 'Mr.Pato-Bot'})
+						  Lxa.sendMessage(from, buffer, image, {quoted: mek, caption: 'TerruBot0.1'})
 						  fs.unlinkSync(ran)
 					  })
 					  break
@@ -3762,7 +3761,7 @@ const {
   
 				  default:
 				  if (body.startsWith(`${prefix}${command}`)) {
-	reply(`        ────────────────\nHols *${pushname}* !!!\nEse comando no esta en mi lista : *${prefix}${command}*\nUsa esto para verlos*${prefix}Menu*\n        ────────────────`)
+	reply(`        ────────────────\nHola mierda, *${pushname}* !!!\nEse comando no esta en mi lista : *${prefix}${command}*\nPor que chucha no lees el menu?*${prefix}Menu*\n        ────────────────`)
 				  }
 					  if (isGroup && isSimi && budy != undefined) {
 						  console.log(budy)
